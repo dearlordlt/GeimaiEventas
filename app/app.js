41affbdc5838f9159bc55b6bf3588dd31981b423
unknown <sarunas79@gmail.com>
2015-10-17 14:24:19 +0300

    ui router

--- app/app.js
+++ app/app.js
@@ -1,1 +1,24 @@
-var app = angular.module("app", []);
+'use strict';
+
+var app = angular.module("app", ['ui.router']);
+
+app.config(['$httpProvider', '$stateProvider', '$urlRouterProvider', function ($httpProvider, $stateProvider, $urlRouterProvider) {
+    //
+    // For any unmatched url, redirect to /state1
+    $urlRouterProvider.otherwise("/home");
+    //
+    // Now set up the states
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: 'components/home/home.html',
+            controller: 'HomeController'
+        })
+        .state('zodis', {
+            url: '/zodis',
+            templateUrl: 'components/zodis/zodis.html',
+            controller: 'ZodisController'
+        });
+
+        $httpProvider.defaults.useXDomain = true;
+}]);
---
+++ app/components/controllers/zodis-ctrl.js
@@ -0,0 +1,3 @@
+angular.module('app', []).controller('ZodisController', ['$scope', function($scope) {
+    $scope.message = "Zodis";
+}]);
