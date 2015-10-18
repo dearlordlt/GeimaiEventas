2fe6d0d1e51faf711753ea3c3158914e485ab923
dearlordlt <sarunas79@gmail.com>
2015-10-18 19:23:56 +0300

    Changed rest services to application/json

--- server/index.js
+++ server/index.js
@@ -3,19 +3,39 @@ var app = express();
 
 var fs = require("fs");
 
+function dataToUsers(data) {
+    var obj = [];
+    var users = data.toString().split(';');
+    for (var i = 0; i < users.length - 1; i++) {
+        var user = users[i].split('|');
+        obj.push({
+            name: user[0],
+            points: user[1],
+            money: user[2]
+        });
+    }
+    return obj;
+}
+
+function dataToWords(data) {
+    return data.toString().split(';');
+}
+
 app.get('/users/', function (req, res) {
     fs.readFile('DB/zaidejai.txt', function (err, data) {
         if (err) throw err;
+        res.charset = 'UTF-8';
         res.set('Content-Type', 'text/plain');
-        res.send(data);
+        res.send(dataToUsers(data));
     });
 });
 
 app.get('/words/', function (req, res) {
     fs.readFile('DB/zodziai.txt', function (err, data) {
         if (err) throw err;
-        res.set('Content-Type', 'text/plain');
-        res.send(data);
+        res.charset = 'UTF-8';
+        res.set('Content-Type', 'application/json');
+        res.send(dataToWords(data));
     });
 });
 
